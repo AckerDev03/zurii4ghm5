bc39f41f3ecfc96eab95ebf0b2226aca99e845e7
Muheez Olubiyi <60267013+Horhluy@users.noreply.github.com>
2022-06-21 22:21:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const FullName = 'Muheez Olubiyi';
+const MyHeight = 6.3;
+const Country = 'Nigeria';
+
+const printOut = "Hello, I'm " + FullName + "...... \nI'm " + MyHeight + " feet tall......\nI'm from " + Country;
+
+console.log(printOut);
+
+document.getElementById('output').innerHTML = printOut;
